50d1dd78ceeb01748add12c928ccbe3c3eba4275
Saksinkarn Petchkuljinda <dward.the.2nd@gmail.com>
2021-04-08 18:32:04 +0700

    add Bottom.js file as well

---
+++ components/Bottom.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+
+const Appbar = props => { 
+    return (
+        <View style = {styles.bottoms}>
+
+        </View>
+
+    );
+};
+
+
+
+const styles = StyleSheet.create({
+    bottoms:{
+        flex: 1
+
+    }
+});
+
+export default Appbar;
