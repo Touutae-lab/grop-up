0ddafb2aafc1db8862de5abe8f6972c567a78e65
Touutae-lab <pantakan.totae@gmail.com>
2021-04-08 19:42:16 +0700

    add bottom components

--- App.js
+++ App.js
@@ -3,18 +3,21 @@ import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
 import Header from './components/Header'
+import Appbar from './components/Bottom'
 
 export default function App() {
   return (
     <View style={styles.screen}>
         <Header/>
+        <Appbar/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   screen: {
-    flex: 1
+    flex: 1,
+    alignContent: 'center'
     },
     logo:{
       flex: 1
