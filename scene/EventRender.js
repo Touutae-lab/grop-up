ca10f791a04ab792084630d94a301574cb8763b2
Saksinkarn Petchkuljinda <dward.the.2nd@gmail.com>
2021-04-10 16:37:45 +0700

    Fix a bug on EventRender.js component, where the keyword 'default' was not supposed to be use when not dealing with the important class, like App, for instance.

--- scene/EventRender.js
+++ scene/EventRender.js
@@ -1,33 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text, StyleSheet} from 'react-native';
 import { render } from 'react-dom';
 
-export default class Apprender extends React.component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
+export class Apprender extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-
-render() {
+  render() {
     return (
-        <View>
-            <Text>{this.props.event}</Text>
-            {/* </View><Image */}
-            {/* source = {require(this.props.img)} /> */}
-            <Text>{this.props.provider}</Text>
-            <Text>{this.props.time}</Text>
-            <Text>{this.props.detail}</Text>
-            <Text>{this.props.place}</Text>
-            <Text>{this.props.tag}</Text>
-        </View>
-        );
-    }
+      <View>
+        <Text>{this.props.event}</Text>
+        {/* </View><Image */}
+        {/* source = {require(this.props.img)} /> */}
+        <Text>{this.props.provider}</Text>
+        <Text>{this.props.time}</Text>
+        <Text>{this.props.detail}</Text>
+        <Text>{this.props.place}</Text>
+        <Text>{this.props.tag}</Text>
+      </View>
+    );
+  }
 }
 
 // const styles = StyleSheet.create({
-//     screen: {
-//         width: windowSize.width * 0.8,
-//         height: windowSize.height * 0.3
-//     }
+//   screen: {
+//     width: windowSize.width * 0.8,
+//     height: windowSize.height * 0.3
+//   }
 // });
